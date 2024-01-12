8bb791b8fcca204724f1faf992dbe87df1454f1e
KazuhiraBenedictMiller <andrea.amedeo.serravalle@protonmail.com>
2024-01-12 04:55:57 +0100

    Model Building

---
+++ scripts/.ipynb_checkpoints/secret-checkpoint.py
@@ -0,0 +1,3 @@
+MariaDB_User = "Zero"
+MariaDB_Password = "Zero.31"
+MariaDB_Database = "Test"
--- scripts/path.py
+++ scripts/path.py
@@ -2,9 +2,14 @@ from pathlib import Path
 import os
 
 PARENT_DIR = Path("__file__").parent.resolve().parent
+MODEL_DIR = PARENT_DIR / "model"
 DATA_DIR = PARENT_DIR / "data"
 RAW_DATA_DIR = PARENT_DIR / "data" / "raw"
 TRANSFORMED_DATA_DIR = PARENT_DIR / "data" / "transformed"
+FEATURES_DATA_DIR = PARENT_DIR / "data" / "features"
+
+if not Path(MODEL_DIR).exists():
+    os.mkdir(MODEL_DIR)
 
 if not Path(DATA_DIR).exists():
     os.mkdir(DATA_DIR)
@@ -14,3 +19,6 @@ if not Path(RAW_DATA_DIR).exists():
 
 if not Path(TRANSFORMED_DATA_DIR).exists():
     os.mkdir(TRANSFORMED_DATA_DIR)
+    
+if not Path(FEATURES_DATA_DIR).exists():
+    os.mkdir(FEATURES_DATA_DIR)
